d792280e45cdf3864c9505c7fc4fe62d0d508939
DedXGIT <shivalikghosh8@gmail.com>
2023-09-03 02:09:01 +0530

    Minor Changes

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -9,11 +9,10 @@ export default function Pricing() {
             {/* <!-- headers content--> */}
             <div className="flex flex-col justify-center items-center text-center">
                 <div className="max-w-sm font-bold font-sans">
-                    Get the most out of your mobile with the right subscription
+                    Pricing as per your requirements
                 </div>
                 <div className="font-light max-w-lg mt-5 text-sm">
-                    All devices come with free delivery or pickup as standard. See information on available shopping
-                    options for your location.
+                    We Have different Packages for different requirements
                 </div>
             </div>
 
@@ -21,15 +20,14 @@ export default function Pricing() {
             <div
                 className="flex flex-col md:flex-row space-x-0 md:space-x-8 space-y-12  md:space-y-0 justify-center items-center mt-10">
                 <div className="bg-[#FFFBEC] rounded-xl">
-                    <div className="flex flex-col p-8 rounded-xl bg-white shadow-xl translate-x-4 translate-y-4 w-96 md:w-auto">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Ice_logo.svg/138px-Ice_logo.svg.png?20191213230535" alt='img' className="w-8"/>
-                        <div className="mt-3 font-semibold text-lg">Ice Mobile 10GB</div>
-                        <div className="text-sm font-light">Up to 100Mbit/s</div>
+                    <div className="flex flex-col p-8 rounded-xl bg-white shadow-xl translate-x-4 translate-y-4 w-96 h-[15.375rem] md:w-auto">
+                        {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Ice_logo.svg/138px-Ice_logo.svg.png?20191213230535" alt='img' className="w-8"/> */}
+                        <div className="mt-3 font-semibold text-2xl">SIMPLE CARD</div>
+                        <div className="flex flex-wrap text-sm font-light">Perfect for people who</div>
+                        <div className="flex flex-wrap text-sm font-light">want simple ID Cards</div>
                         <div className="my-4">
-                            <span className="font-bold text-base">299,-</span>
-                            <span className="font-light text-sm">/month</span>
+                            <span className="font-abc font-bold text-4xl">$ 1499 /-</span>
                         </div>
-
                         <button className="bg-[#F4F5FA] px-4 py-3 rounded-full  border border-[#F0F0F6] shadow-xl mt-4">
                             Add subscription
                         </button>
@@ -37,9 +35,10 @@ export default function Pricing() {
                 </div>
 
                 <div className="bg-[#F9ECFF] rounded-xl">
-                    <div className="flex flex-col p-8 rounded-xl bg-white shadow-xl translate-x-4 translate-y-4 w-96 md:w-auto">
-                        <img src="https://www.dstny.se/app/uploads/telia_pp_rgb.png.webp" alt='img' className="w-12"/>
-                        <div className="mt-3 font-semibold text-lg">Telia Mobil 15GB</div>
+                    <div className="flex flex-col p-8 rounded-xl bg-white shadow-xl translate-x-4 translate-y-4 w-96 h-[15.375rem] md:w-auto">
+                        {/* <img src="https://www.dstny.se/app/uploads/telia_pp_rgb.png.webp" alt='img' className="w-12"/> */}
+                        <div className="mt-3 font-semibold text-lg">ADVANCED CARD</div>
+                        <div className="text-sm font-light w-60 md:w-auto">Unlimited calls</div>
                         <div className="text-sm font-light w-60 md:w-auto">Unlimited calls</div>
                         <div className="my-4">
                             <span className="font-bold text-base">953,-</span>
@@ -54,9 +53,10 @@ export default function Pricing() {
 
 
                 <div className="bg-[#ECEEFF] rounded-xl">
-                    <div className="flex flex-col p-8 rounded-xl bg-white shadow-xl translate-x-4 translate-y-4 w-96 md:w-auto">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Telenor_Logo.svg/1600px-Telenor_Logo.svg.png" alt='img' className="w-12"/>
-                        <div className="mt-3 font-semibold text-lg">Telenor Next Fast</div>
+                    <div className="flex flex-col p-8 rounded-xl bg-white shadow-xl translate-x-4 translate-y-4 w-96 h-[15.375rem] md:w-auto">
+                        {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Telenor_Logo.svg/1600px-Telenor_Logo.svg.png" alt='img' className="w-12"/> */}
+                        <div className="mt-3 font-semibold text-lg">LEGENDARY CARD</div>
+                        <div className="text-sm font-light w-60 md:w-auto">Up to 100Mbit/s</div>
                         <div className="text-sm font-light w-60 md:w-auto">Up to 100Mbit/s</div>
                         <div className="my-4">
                             <span className="font-bold text-base">1028,-</span>
